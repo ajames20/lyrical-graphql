a091b129683bb78aab8f26e56de3aae71faec6b0
Andrew James <ajames20@gmail.com>
2017-12-19 09:18:25 +0200

    add router, apolloprovider

--- client/index.js
+++ client/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
 const Root = () => (
   <ApolloProvider client={client}>
-    <div>Lyrical</div>
+    <Router history={hashHistory}>
+      <Router path="/" component={App}>
+        <IndexRoute component={SongList} />
+      </Router>
+    </Router>
   </ApolloProvider>
 );
 
