3161469b5a092fc36190815443f0dd1d5ee3ea0d
Andrew James <ajames20@gmail.com>
2017-12-20 11:15:47 +0200

    add likeLyric funcitonality

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/addLike';
 
 class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderLyrics() {
     if (!this.props.lyrics.length) {
       return <li className="collection-item">Please Add Lyrics</li>;
     }
-    return this.props.lyrics.map(({ id, content }) => {
+
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li className="collection-item" key={id}>
           {content}
+          <div className="vote-box">
+            {likes}
+            <i className="material-icons" onClick={() => this.onLike(id)}>
+              thumb_up
+            </i>
+          </div>
         </li>
       );
     });
@@ -18,4 +31,4 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
