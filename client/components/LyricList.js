09113223893a80a52ee88e41671b2623276bc4da
Andrew James <ajames20@gmail.com>
2017-12-20 10:38:26 +0200

    add lyrics content and id to mutation so graphql can track id

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 class LyricList extends Component {
   renderLyrics() {
+    if (!this.props.lyrics.length) {
+      return <li className="collection-item">Please Add Lyrics</li>;
+    }
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li className="collection-item" key={id}>
--- client/mutations/addLyric.js
+++ client/mutations/addLyric.js
@@ -4,6 +4,10 @@ export default gql`
   mutation AddLyricToSong($content: String, $songId: ID) {
     addLyricToSong(content: $content, songId: $songId) {
       id
+      lyrics {
+        id
+        content
+      }
     }
   }
 `;
